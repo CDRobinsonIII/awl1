22cc32dd5313a9f02fe759de5cd39b5933bd24f2
Niccolo Ortega <nfcortega89@gmail.com>
2018-01-13 15:45:30 -0800

    added spring art camp pics

--- src/containers/ArtCamps.js
+++ src/containers/ArtCamps.js
@@ -7,6 +7,9 @@ import Pic3 from "../assets/Student Creating Art_03.jpg";
 import Pic4 from "../assets/Student Creating Art_02.jpg";
 import Pic5 from "../assets/Art Camp Project_09.jpg";
 import Pic6 from "../assets/Art Camp Project_08.jpg";
+import Pic7 from "../assets/Art Camp Project_07.JPG";
+import Pic8 from "../assets/Art Camp Project_14.jpg";
+import Pic9 from "../assets/Art Camp Project_04.jpg";
 
 class ArtCamps extends Component {
   render() {
@@ -31,9 +34,9 @@ class ArtCamps extends Component {
             body="Learn how to draw fine art and create new crafts everyday. The camp are for 4 days (no camp on Tuesdays). The fun begins at 10am and lasts until 1pm. We will still have afternoon classes during camp weeks. All the supplies are included as well as snacks. The cost is 150 for 4 days. Each art camp is based on availability, and you must reserve your spot with a $50 art camp deposit. Please email or call us for details."
           />
           <ArtCampCard
-            leftUpperimage={Pic3}
-            leftLowerImage={Pic2}
-            rightLowerImage={Pic1}
+            leftUpperimage={Pic7}
+            leftLowerImage={Pic8}
+            rightLowerImage={Pic9}
             header="Spring Art Camps"
             body="Learn how to draw fine art and create new crafts everyday. The camp are for 4 days (no camp on Tuesdays). The fun begins at 10am and lasts until 1pm. We will still have afternoon classes during camp weeks. All the supplies are included as well as snacks. The cost is 150 for 4 days. Each art camp is based on availability, and you must reserve your spot with a $50 art camp deposit. Please email or call us for details."
           />
