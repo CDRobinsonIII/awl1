f91889cbf928570c97498f2b744fa7b586757097
Niccolo Ortega <nfcortega89@gmail.com>
2018-02-13 14:53:22 -0800

    converted LocationBlock to styled-components

--- src/components/LocationBlock.js
+++ src/components/LocationBlock.js
@@ -1,23 +1,65 @@
-import React, { Component } from "react";
-import "../styles/locationblock.css";
-import LocationCard from "../components/LocationCard";
-import Bonita from "../assets/bonita.png";
-import Lamesa from "../assets/lamesa.png";
-import Santee from "../assets/santee.png";
-import Blank from "../assets/blank.png";
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import LocationCard from '../components/LocationCard';
+import Bonita from '../assets/bonita.png';
+import Lamesa from '../assets/lamesa.png';
+import Santee from '../assets/santee.png';
+import Blank from '../assets/blank.png';
+import head from '../assets/location-block-background.png';
+import body from '../assets/secondary-background-tile.png';
 
+const LocationWrapper = styled.section`
+  width: 100%;
+`;
+
+const LocationHeader = styled.div`
+  text-align: center;
+  background-position-y: -9px;
+  height: 332px;
+  margin-top: 40px;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  color: white;
+  width: 100%;
+  background-image: url(${head});
+`;
+const LocationH1 = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: 1em;
+  font-size: 2em;
+  color: white;
+  -webkit-font-smoothing: antialiased;
+  font-family: 'AvantGardeMdITC';
+`;
+
+const LocationBody = styled.div`
+  background-image: url(${body});
+  display: flex;
+  width: 100%;
+  align-items: center;
+  flex-direction: column;
+  height: 130vh;
+`;
+
+const LocationRow = styled.div`
+  display: flex;
+  padding: 2em 0;
+`;
 class LocationBlock extends Component {
   render() {
     return (
-      <div className="locationBlock-wrapper">
-        <div className="locations-tag__container">
-          <h1 className="locations-tag__text">
+      <LocationWrapper>
+        <LocationHeader>
+          <LocationH1>
             Click the class schedule icon to <br /> select your prefered day and
             time
-          </h1>
-        </div>
-        <div className="locations-body">
-          <div className="location-upper">
+          </LocationH1>
+        </LocationHeader>
+        <LocationBody>
+          <LocationRow>
             <LocationCard
               backgroundColor="rgba(73, 38, 73, 0.7"
               image={Bonita}
@@ -34,8 +76,8 @@ class LocationBlock extends Component {
               city="San Diego, CA 92111"
               number="(619)-456-7506"
             />
-          </div>
-          <div className="location-lower">
+          </LocationRow>
+          <LocationRow>
             <LocationCard
               image={Lamesa}
               backgroundColor="rgba(235, 101, 89, 0.7)"
@@ -52,9 +94,9 @@ class LocationBlock extends Component {
               city="Santee, CA 92701"
               number="(619)-855-2787"
             />
-          </div>
-        </div>
-      </div>
+          </LocationRow>
+        </LocationBody>
+      </LocationWrapper>
     );
   }
 }
