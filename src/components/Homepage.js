789d6d14e38b293498eaf23508596aa8a3d36e18
Niccolo Ortega <nfcortega89@gmail.com>
2018-05-03 17:45:18 -0700

    took out links

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import styled from 'styled-components';
 import LocationCard from './LocationCard';
 import Slider from '../containers/Slider.js';
